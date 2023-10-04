7ff25afaebc52002c98b89c85fe385847432412c
Rafli Akhmad Baihaqi <baihaqirafli30@gmail.com>
2023-10-04 13:47:02 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+// routes.js: Memuat kode konfigurasi routing server, seperti menentukan path, method, dan handler yang digunakan.
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handlers');
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
